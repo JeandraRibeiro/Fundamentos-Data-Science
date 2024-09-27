81922503c6846c58b89f4b7e3201bafd4351a375
Jeandra <76909903+Jeandra@users.noreply.github.com>
2024-09-27 15:35:17 -0300

    teste 3

---
+++ teste.py
@@ -0,0 +1,12 @@
+users = [
+    { "id": 0, "name": "Jeandra"},
+    { "id": 1, "name": "Alex"},
+    { "id": 2, "name": "Hugo"},
+    { "id": 3, "name": "Jadna"},
+    { "id": 4, "name": "Karina"},
+    { "id": 5, "name": "Ysa"},
+    { "id": 6, "name": "Everton"},
+    { "id": 7, "name": "Raquel"},
+    { "id": 8, "name": "Vanja"},
+    { "id": 9, "name": "Veronica"}
+]
