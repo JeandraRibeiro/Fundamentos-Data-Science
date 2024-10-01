963c2a4223278f2db6e86ad2ff4dde99688068ce
Jeandra <76909903+Jeandra@users.noreply.github.com>
2024-10-01 15:47:19 -0300

    Criando o dict

--- Encontrando_Conectores-Chaves.py
+++ Encontrando_Conectores-Chaves.py
@@ -13,3 +13,11 @@ users = [
 
 friendship_pairs = [(0, 1), (0,2), (1, 2), (1, 3), (2, 3), (3, 4),
                     (4, 5), (5, 6), (5, 7), (6, 8), (7, 8), (8, 9)]
+
+# Inicializando o dict com uma lista vazia para cada id de usuário:
+friendships = {user["id"]: [] for user in users} 
+
+# Agora execute um loop pelos pares de amigos para preenchê-las:
+for i, j in friendship_pairs:
+    friendships[i].append(j) # Adiciona j como amigo do usuário i
+    friendships[j].append(i) # Adiciona i como amigo do usuário j
